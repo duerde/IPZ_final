53889580364c5ded35b57f6f2ed34f5b4a0c2f3c
Marcia <marta.glanowska@outlook.com>
2021-04-15 16:46:16 +0200

    textViews fixed

--- app/src/main/java/com/example/ipz_final/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/ipz_final/ui/notifications/NotificationsFragment.java
@@ -42,12 +42,15 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.textView);
+        /*
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        }
+        );
+        */
 
         //preparing data
         SetData();
