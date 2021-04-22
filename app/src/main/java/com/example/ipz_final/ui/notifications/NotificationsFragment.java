a27a62a80ed60920aab4b08afa4f2457f8d44c78
sebkuc <sebastian.kuc70@wp.pl>
2021-04-22 18:29:57 +0200

    pop1

--- app/src/main/java/com/example/ipz_final/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/ipz_final/ui/notifications/NotificationsFragment.java
@@ -36,6 +36,7 @@ public class NotificationsFragment extends Fragment {
     BarDataSet barDataSetEko;
     BarDataSet barDataSetKm;
     ArrayList barEntriesRides;
+    ArrayList barEntriesEko;
 
     private NotificationsViewModel notificationsViewModel;
 
@@ -57,7 +58,8 @@ public class NotificationsFragment extends Fragment {
         */
 
         //preparing data
-        SetData();
+        SetDataKm();
+        SetDataEko();
         ArrayList<String> labels= new ArrayList<String>();
         labels.add("I");
         labels.add("II");
@@ -77,7 +79,7 @@ public class NotificationsFragment extends Fragment {
 
         //barChart Eko
         barChartEko=root.findViewById(R.id.BarChart2);
-        barDataSetEko = new BarDataSet(barEntriesRides, "labels");
+        barDataSetEko = new BarDataSet(barEntriesEko, "labels");
         barDataEko = new BarData(barDataSetEko);
         barDataSetEko.setColors(ColorTemplate.PASTEL_COLORS);
         barDataSetEko.setValueTextColor(Color.BLACK);
@@ -88,16 +90,18 @@ public class NotificationsFragment extends Fragment {
         return root;
     }
 
-    private void SetData(){
+    private void SetDataKm(){
         barEntriesRides = new ArrayList<>();
-        barEntriesRides.add(new BarEntry(1f,0));
-        barEntriesRides.add(new BarEntry(4f,1));
-        barEntriesRides.add(new BarEntry(7f,2));
-        barEntriesRides.add(new BarEntry(8f,3));
-
-
-
-
+        barEntriesRides.add(new BarEntry(1,50));
+        barEntriesRides.add(new BarEntry(2,100));
+        barEntriesRides.add(new BarEntry(3,250));
+        barEntriesRides.add(new BarEntry(4,30));
+    }
+    private void SetDataEko(){
+        barEntriesEko = new ArrayList<>();
+        //odwołanie do listy Karoliny
+        //odwołanie do listy Daniela
+        //pętla z obliczeniami i dodawanie entries
     }
 
 }
